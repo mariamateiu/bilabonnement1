0ffb8d49695db4af65adfeb0a482d682346b8a50
mariamateiu <mari1b71us@gmail.com>
2022-05-03 10:11:42 +0200

    test

---
+++ src/main/java/com/example/bilabonnement1/Bilabonnement1Application.java
@@ -0,0 +1,13 @@
+package com.example.bilabonnement1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bilabonnement1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bilabonnement1Application.class, args);
+    }
+
+}
