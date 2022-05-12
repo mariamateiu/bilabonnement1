411fc910f422030a221e1a9b4c8f5daf473d7e7e
mariamateiu <mari1b71us@gmail.com>
2022-05-12 13:46:21 +0200

    idk

--- src/main/java/Model/Employee.java
+++ src/main/java/Model/Employee.java
@@ -3,11 +3,13 @@ package Model;
 public class Employee {
     private String username;
     private String password;
+    private int employeeID;
 
 
-    public Employee(String username, String password) {
+    public Employee(String username, String password, int employeeID) {
         this.username = username;
         this.password = password;
+        this.employeeID = employeeID;
     }
 
     public String getUsername() {
@@ -28,11 +30,20 @@ public class Employee {
         this.password = password;
     }
 
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+
     @Override
     public String toString() {
         return "Employee{" +
                 "username='" + username + '\'' +
                 ", password='" + password + '\'' +
+                ", employeeID=" + employeeID +
                 '}';
     }
 }
